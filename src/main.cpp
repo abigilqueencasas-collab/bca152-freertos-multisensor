02e25c0353af21cc0f68eac9ee6fd77be2052a9d
Abigil Queen Casas <abigilqueen.casas@g.msuiit.edu.ph>
2026-09-21 20:53:03 +0800

    Configure initial Wokwi simulation

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,5 @@
 #include <cstdio>
-extern "C" void app_main(void) { printf("Hello\n"); }
+extern "C" void app_main(void) {
+    printf("BCA152 FreeRTOS Multisensor\n");
+    printf("System starting...\n");
+}
